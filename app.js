e78038c1710b8018b5e65f8946e6f446cc5c3926
vento1982 <marcin.vento@gmail.com>
2017-10-07 10:24:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//BUDGET CONTROLLER
+var budgetController =  ( function() {
+    
+    //SOME CODE
+   
+})();
+
+
+
+//UI CONTROLLER
+var UIController = ( function() {
+    // Some code
+})();
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = ( function(budgetCtrl, uiCtrl) {
+    
+var addBtn = document.querySelector('.add__btn');
+addBtn.addEventListener('click', function(){
+    
+    // 1. Get the field input data
+    
+    
+    // 2. Add the item to the budget controller
+    
+    
+    // 3. Add the item to the UI
+    
+    
+    // 4. Calculate the budget
+    
+    
+    // 5. Update the budget on the UI
+});
+
+    
+})(budgetController, UIController);
